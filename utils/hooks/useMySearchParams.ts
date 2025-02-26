b8279f0891d85e87a62d0b47d68eb3312e8cd08b
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2025-02-27 02:21:37 +0700

    param fix

--- utils/hooks/useMySearchParams.ts
+++ utils/hooks/useMySearchParams.ts
@@ -1,7 +1,33 @@
 
-const searchParams = new URLSearchParams(location.search)
 
+
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 
 export const useMySearchParams = () => {
-    return searchParams
+    const searchParams = useSearchParams()
+    
+    return useMemo(() => {
+        return {
+            get: (key: string) => {
+                // If searchParams is not available during SSR, return null
+                if (!searchParams) return null
+                return searchParams.get(key)
+            },
+            getAll: (key: string) => {
+                if (!searchParams) return []
+                return searchParams.getAll(key)
+            },
+            has: (key: string) => {
+                if (!searchParams) return false
+                return searchParams.has(key)
+            },
+            toString: () => {
+                if (!searchParams) return ''
+                return searchParams.toString()
+            }
+        }
+    }, [searchParams])
 }
